d0f5857da8627ffcdc88ba37d9a353e4d01eae94
Som3on31 <saharit.f4@gmail.com>
2021-08-26 14:06:25 +0700

    modified Navbar to have links to each route

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,72 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <div>
+      <Router>
+        <Navbar />
 
-      {/* template for /current */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Current price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
-        <p> (Last updated) </p>
-      </div>
 
-      <br />
+        <Switch>
+          <Route path='/'>
 
-      {/* template for /history/select */}
-      <div className='text-center space-y-3 space-x-3'>
-        <p className='text-2xl font-semibold'>Select historical range</p>
-        <span>From date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
-        <span>To date</span>
-        <input type='date' onChange={e => console.log(e.target.value)}></input>
+          </Route>
+          <Route path='/current'>
+
+          </Route>
+          <Route path='/history/select'>
+
+          </Route>
+          <Route path='/about'>
+
+          </Route>
+        </Switch>
+        {/* template for /current */}
+        <div className='text-center space-y-3'>
+          <p className='text-2xl font-semibold'>Current price</p>
+          <p className='text-2xl'>Loading ...</p>
+          <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
+          <p> (Last updated) </p>
+        </div>
+
         <br />
-        <button>Get data</button>
-      </div>
 
-      <br />
+        {/* template for /history/select */}
+        <div className='text-center space-y-3 space-x-3'>
+          <p className='text-2xl font-semibold'>Select historical range</p>
+          <span>From date</span>
+          <input type='date' onChange={e => console.log(e.target.value)}></input>
+          <span>To date</span>
+          <input type='date' onChange={e => console.log(e.target.value)}></input>
+          <br />
+          <button>Get data</button>
+        </div>
 
-      {/* template for /history/result */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>Historical price</p>
-        <p className='text-2xl'>Loading ...</p>
-        <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
-        <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
-        <ul>
-          <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
-          <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
-        </ul>
-      </div>
+        <br />
 
-      <br />
+        {/* template for /history/result */}
+        <div className='text-center space-y-3'>
+          <p className='text-2xl font-semibold'>Historical price</p>
+          <p className='text-2xl'>Loading ...</p>
+          <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
+          <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
+          <ul>
+            <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
+            <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
+            <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
+          </ul>
+        </div>
 
-      {/* template for about me */}
-      <div className='text-center space-y-3'>
-        <p className='text-2xl font-semibold'>About me</p>
-        <p className='text-xl'>Chayanin Suatap 610631100</p>
-      </div>
+        <br />
 
-    </Router>
+        {/* template for about me */}
+        <div className='text-center space-y-3'>
+          <p className='text-2xl font-semibold'>About me</p>
+          <p className='text-xl'>Saharit Kadkasem 630610767</p>
+        </div>
+
+      </Router>
+    </div>
+
   );
 }
 
