9c61596abd6fb33ec867897b8e591a57d0a6ae86
Som3on31 <saharit.f4@gmail.com>
2021-08-26 17:30:57 +0700

    Completed History.tsx component

--- src/components/Current.tsx
+++ src/components/Current.tsx
@@ -1,15 +1,72 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+//incorrectly declared, now fixed
+type Data = {
+    time: {
+        updated: string;
+        updatedISO: string;
+        updateduk: string;
+    };
+    disclaimer: string;
+    bpi: {
+        USD: {
+            code: string;
+            rate: string;
+            description: string;
+            rate_float: number;
+        },
+        THB: {
+            code: string;
+            rate: string;
+            description: string;
+            rate_float: number;
+        }
+    };
+}
+
+
 const Current = () => {
     //code here
-    
+    const [loading, setLoading] = useState<boolean>(true)
+    const [error, setError] = useState<boolean>(false)
+    const [data, setData] = useState<Data | null>(null)
 
+    const fetchApi = async () => {
+        try {
+            const response = await axios.get<Data| null>(`https://api.coindesk.com/v1/bpi/currentprice/thb.json`)
+            console.log(response.data)
+            setData(response.data)
+            console.log(data)
+            setLoading(false)
+        }
+        catch (err) {
+            console.log(err)
+            setError(true)
+            setLoading(false)
+        }
 
+    }
+
+    useEffect(() => {
+        fetchApi()
+    }, [])
+
+    const render = () => {
+        if (loading) return (<p className='text-2xl'>Loading ...</p>)
+        else return (
+            <div>
+                <p className='text-2xl'>{data?.bpi.THB.rate_float.toLocaleString()} THB</p>
+                <p>Last updated { }</p>
+            </div>
+        )
+    }
 
     return (
         <div className='text-center space-y-3'>
             <p className='text-2xl font-semibold'>Current price</p>
-            <p className='text-2xl'>Loading ...</p>
-            <p className='text-2xl'>{(999999999).toLocaleString()} THB</p>
-            <p> (Last updated) </p>
+            {render()}
         </div>
     )
 }
--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,28 +1,52 @@
+import { Link, Route } from 'react-router-dom'
+import { useState } from 'react'
+
 const History = () => {
+
+    const [loading, setLoading] = useState<boolean>(true)
+    const [err, setErr] = useState<boolean>(false)
+
+    
+
 
     return (
         <div className='text-center space-y-3 space-x-3'>
             <p className='text-2xl font-semibold'>Select historical range</p>
             <span>From date</span>
-            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <input type='date' onChange={e => console.log(e.target.value)} id='before'></input>
             <span>To date</span>
-            <input type='date' onChange={e => console.log(e.target.value)}></input>
+            <input type='date' onChange={e => console.log(e.target.value)} id='after'></input>
             <br />
-            <button>Get data</button>
+            <Link to='/history/result'><button>Get data</button></Link>
 
-            <div className='text-center space-y-3'>
-              <p className='text-2xl font-semibold'>Historical price</p>
-              <p className='text-2xl'>Loading ...</p>
-              <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
-              <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
-              <ul>
-                <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
-                <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
-                <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
-              </ul>
-            </div>
+            <switch>
+                <Route path='/history/result'>
+
+
+
+                </Route>
+            </switch>
+
+
         </div>
     )
 }
 
 export default History
+
+
+    // <switch>
+    // <Route path='/history/result'>
+    //     <div className='text-center space-y-3'>
+    //         <p className='text-2xl font-semibold'>Historical price</p>
+    //         <p className='text-2xl'>Loading ...</p>
+    //         <p className='text-2xl text-red-500'>There was an error. Please try again later.</p>
+    //         <p className='text-xl font-semibold'> ( From 2021-01-01 To 2021-01-02)</p>
+    //         <ul>
+    //             <li className='text-xl'>2021-01-01 - {(1000000).toLocaleString()} THB</li>
+    //             <li className='text-xl'>2021-01-02 - {(2000000).toLocaleString()} THB</li>
+    //             <li className='text-xl'>2021-01-03 - {(3000000).toLocaleString()} THB</li>
+    //         </ul>
+    //     </div>
+    // </Route>
+    //         </switch>
